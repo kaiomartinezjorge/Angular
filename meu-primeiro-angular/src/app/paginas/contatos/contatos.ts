ad7cb3ba622a4dc117de06824e9b41ab47ec8c2b
kaiomartinezjorge <kaiomartinez831@gmail.com>
2026-05-26 17:11:15 -0300

    Atividade final da aula

--- meu-primeiro-angular/src/app/paginas/contatos/contatos.ts
+++ meu-primeiro-angular/src/app/paginas/contatos/contatos.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './contatos.html',
   styleUrl: './contatos.css',
 })
-export class Contatos {}
+export class Contatos {
+
+  nome: string = "";
+  email: string = "";
+  mensagem: string = "";
+  valorsalvo: string = "";
+
+  onKeyUp(event: any){
+    this.nome = event.target.value;
+  }
+
+  onKeyUpEmail(event: any){
+    this.email = event.target.value;
+  }
+
+  onKeyUpMensagem(event: any){
+    this.mensagem = event.target.value;
+  }
+
+  salvarValor(valor:string){
+    this.valorsalvo = valor;
+  }
+}
